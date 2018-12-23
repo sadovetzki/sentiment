96cb33dd1e3bbe988466d11fe30cf23e0906b408
Stefan Sadovek <stefan.sadovek@apexsql.com>
2018-12-23 13:42:50 +0100

    updated global styling and app.component

--- SentimentApp-SPA/src/app/app.module.ts
+++ SentimentApp-SPA/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LexComponent } from './lex/lex.component';
+import { LexService } from './_services/lex.service';
+import { CalcService } from './_services/calc.service';
+import { CalculateComponent } from './calculate/calculate.component';
+import { LexiconComponent } from './lexicon/lexicon.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonsModule, TabsModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LexComponent,
+    CalculateComponent,
+    LexiconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    TabsModule.forRoot(),
+    ButtonsModule.forRoot()
   ],
-  providers: [],
+  providers: [LexService, CalcService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
